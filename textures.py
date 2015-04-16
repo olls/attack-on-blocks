0cd61dcb482e3241a5991ff1f09fc6e527836852
Jake Howard <git@theorangeone.net>
2015-04-16 13:23:15 +0100

    added load texturepack function, or at least started it!

--- textures.py
+++ textures.py
@@ -1,16 +1,23 @@
-import os
+import os, pygame, glob
 class Textures():
 	def __init__(self):
 		self.images = {
 			"PLAYER":"player.png",
 			"BULLET":"bullet.png",
-			"TARGET":[]
+			"TARGETS":[]
 		}
-		self.path=os.path.dirname(os.path.realpath(__file__)) + "\\resources\\texture_packs"
-		self.pac
-	def loadTexturePack(self, filename):
-		pass
-	def getTexture(self, object):
-		directory =
+		self.path=os.path.dirname(os.path.realpath(__file__)) + "\\resources\\texture_packs\\"
+		self.pack = "default"
 
-tex = Textures()
+	def loadTexturePack(self, packName):
+		if os.path.exists(self.path + packName):
+			targets = glob.glob(self.path+packName+"\\target*.png")
+			for file in files:
+				fileName = file.split("\\")[-1]
+				if 
+			self.pack = packName
+
+	def getTexture(self, objectName):
+		filename = self.path + self.pack + "\\{0}.png".format(self.images[objectName.upper()])
+		return pygame.image.load(filename)
+
