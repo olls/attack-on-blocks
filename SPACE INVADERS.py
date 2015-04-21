3d85a34dd851692092ab6d384cdbfa2d76bb241c
Jake Howard <git@theorangeone.net>
2015-04-21 11:00:44 +0100

    Imported GUI to main file.

--- SPACE INVADERS.py
+++ SPACE INVADERS.py
@@ -1,4 +1,5 @@
 import pygame, logging
-import bullet, player, textures # Import other files
+import bullet, player, textures, GUI # Import other files
 
 logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.DEBUG)
+
