69582a5eaa98445a0ad2bc12099be021a15a27cf
Jake Howard <git@theorangeone.net>
2015-04-29 09:48:37 +0100

    updated movement

--- player.py
+++ player.py
@@ -2,14 +2,19 @@ import pygame
 
 
 class Shooter(pygame.sprite.Sprite):
-    def __init__(self, color=(30,0,150), width=64, height=64):
+    def __init__(self, window, color=(30,0,150), width=50, height=25):
         super().__init__()
+        self.width = width
+        self.height = height
         self.image = pygame.Surface((width, height))
         self.image.fill(color)
         self.rect = self.image.get_rect()
         self.speed = 4
+        self.window_rect = window.get_rect()
         
     def set_position(self,x,y):
          self.rect.x, self.rect.y = x,y
          
-    def move(self, value): self.rect.x += value
+    def move(self, value): 
+    	self.rect.x += value
+    	self.rect.clamp_ip(self.window_rect)
