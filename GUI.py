ae52af742652d5af2553290bb509448ea1ec4bad
Jake Howard <git@theorangeone.net>
2015-05-11 13:01:56 +0100

    added textures to options

--- GUI.py
+++ GUI.py
@@ -54,8 +54,10 @@ class Main_Window:
 
 class Options_Window:
     def __init__(self):
+    	self.textures = Textures()
         self.options = {
-            "Difficulty": 120
+            "Difficulty": 120,
+            "Textures": self.textures
         }
 
     def display(self, master):
